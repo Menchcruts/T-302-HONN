4ae56d4064a5b2fb2ea35c34e32293972a56ab39
Menchcruts <joelarnar@gmail.com>
2025-08-24 13:38:11 +0000

    Kláraði forritunar hlutann.

--- Labs/Lab1/filelogger.py
+++ Labs/Lab1/filelogger.py
@@ -6,17 +6,18 @@ class FileLogger(Logger):
         super().__init__()
         self.log_file = Path("./logging.log")
     
-    def log_info(self, message):
+    def __log(self, message:str) -> None:
         with self.log_file.open("a+") as file:
-            file.write(f"info: {message}\n")
+            file.write(message)
+        
+    def log_info(self, message):
+        self.__log(f"info: {message}\n")
         return super().log_info(message)
 
     def log_warning(self, message):
-        with self.log_file.open("a+") as file:
-            file.write(f"warning: {message}\n")
+        self.__log(f"warning: {message}\n")
         return super().log_warning(message)
     
     def log_error(self, message, exception):
-        with self.log_file.open("a+") as file:
-            file.write(f"warning: {message}, exception: {exception}\n")
+        self.__log(f"warning: {message}, exception: {exception}\n")
         return super().log_error(message, exception)
