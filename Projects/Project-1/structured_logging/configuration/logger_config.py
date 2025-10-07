5c08889dd5da6eea4beab50edd66ea7cc092188f
Menchcruts <joelarnar@gmail.com>
2025-10-07 19:33:16 +0000

    Altered LoggerConfig to use default params.

--- Projects/Project-1/structured_logging/configuration/logger_config.py
+++ Projects/Project-1/structured_logging/configuration/logger_config.py
@@ -2,9 +2,12 @@ from structured_logging.processors.i_processor import IProcessor
 from structured_logging.sinks.i_sink import ISink
 from pydantic import (BaseSettings)
 
+from structured_logging.sinks.console_sink import ConsoleSink
+from structured_logging.processors.null_processor import NullProcessor
+
 
 class LoggerConfig(BaseSettings):
-    sink: ISink
-    processor: IProcessor
-    is_async: bool
-    async_wait_delay_in_seconds: int
+    sink: ISink                         = ConsoleSink()
+    processor: IProcessor               = NullProcessor()
+    is_async: bool                      = False
+    async_wait_delay_in_seconds: int    = 0
