b930700a58a147af817000cf091b4d072f5d65f6
Menchcruts <joelarnar@gmail.com>
2025-10-07 20:17:45 +0000

    Finished sinks

--- Projects/Project-1/structured_logging/sinks/console_sink.py
+++ Projects/Project-1/structured_logging/sinks/console_sink.py
@@ -1,17 +1,20 @@
 from structured_logging.sinks.i_sink import ISink
+import json
 
 class ConsoleSink(ISink):
     def sink_data(self, data):
-        data_len = len(data)
-        i = 0
-        strings: list[str] = ["{"]
+        # data_len = len(data)
+        # i = 0
+        # strings: list[str] = ["{"]
 
-        for key, value in data.items():
-            i += 1
-            string = f'  "{key}": "{value}"'
-            if i < data_len:
-                string += ","
-            strings.append(string)
-        strings.append("}")
+        # for key, value in data.items():
+        #     i += 1
+        #     string = f'  "{key}": "{value}"'
+        #     if i < data_len:
+        #         string += ","
+        #     strings.append(string)
+        # strings.append("}")
 
-        print("\n".join(strings))
+        # print("\n".join(strings))
+
+        print(json.dumps(data, indent=2, ensure_ascii=False))
--- Projects/Project-1/structured_logging/sinks/file_sink.py
+++ Projects/Project-1/structured_logging/sinks/file_sink.py
@@ -6,17 +6,24 @@ class FileSink(ISink):
     def __init__(self, filepath: Path):
         super().__init__()
         self.path = Path(filepath)
+        if self.path.exists() and not self.path.is_file():
+            raise ValueError("FileSink: 'filepath' must be a file.")
+        self.path.parent.mkdir(exist_ok=True, parents=True)
 
     def get_data(self) -> list[dict]:
         data = []
-        with self.path.open("r") as file:
-            data = json.load(file)
+        with self.path.open("a+", encoding="utf-8") as file:
+            try:
+                data = json.load(file)
+            except json.decoder.JSONDecodeError as e:
+                pass
         return data
     
     def save_data(self, data: list[dict]) -> None:
-        with self.path.open("w+") as file:
-            json.dump(data, file)
+        with self.path.open("w+", encoding="utf-8") as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
 
     def sink_data(self, data):
         saved_data = self.get_data()
         saved_data.append(data)
+        self.save_data(saved_data)
