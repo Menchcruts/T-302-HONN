8d670cf6219035878c54db21de8e0c198272dff3
Menchcruts <joelarnar@gmail.com>
2025-10-09 22:42:53 +0000

    LoggerConfig should be ready.

--- Projects/Project-1/structured_logging/configuration/logger_config.py
+++ Projects/Project-1/structured_logging/configuration/logger_config.py
@@ -2,12 +2,9 @@ from structured_logging.processors.i_processor import IProcessor
 from structured_logging.sinks.i_sink import ISink
 from pydantic import (BaseSettings)
 
-from structured_logging.sinks.console_sink import ConsoleSink
-from structured_logging.processors.null_processor import NullProcessor
-
 
 class LoggerConfig(BaseSettings):
-    sink: ISink                         = ConsoleSink()
-    processor: IProcessor               = NullProcessor()
-    is_async: bool                      = False
-    async_wait_delay_in_seconds: int    = 0
+    sink: ISink
+    processor: IProcessor
+    is_async: bool
+    async_wait_delay_in_seconds: int
--- Projects/Project-1/structured_logging/logger_creation/logger_config_builder.py
+++ Projects/Project-1/structured_logging/logger_creation/logger_config_builder.py
@@ -1,33 +1,73 @@
 from structured_logging.configuration.environment import Environment
 from structured_logging.configuration.logger_config import LoggerConfig
+
 from structured_logging.processors.i_processor import IProcessor
+from structured_logging.processors.null_processor import NullProcessor
+
 from structured_logging.sinks.i_sink import ISink
+from structured_logging.sinks.file_sink import FileSink
+from structured_logging.sinks.console_sink import ConsoleSink
+
 
 
 class LoggerConfigBuilder:
+    __sink: ISink
+    __processors: list[IProcessor]
+    __is_async: bool
+    __async_wait_delay_in_seconds: int
+    __environment: Environment
+
     def __init__(self):
-        raise NotImplementedError()
+        self._clear()
 
     def with_custom_sink(self, sink: ISink) -> 'LoggerConfigBuilder':
-        raise NotImplementedError()
-
+        assert isinstance(sink, ISink)
+        self.__sink = sink
+        return self
+        
     def with_file_sink(self, file_path: str) -> 'LoggerConfigBuilder':
-        raise NotImplementedError()
+        self.__sink = FileSink(file_path)
+        return self
 
     def with_console_sink(self) -> 'LoggerConfigBuilder':
-        raise NotImplementedError()
+        self.__sink = ConsoleSink()
+        return self
 
     def as_async(self, wait_delay_in_seconds: int) -> 'LoggerConfigBuilder':
-        raise NotImplementedError()
+        self.__is_async = True
+        self.__async_wait_delay_in_seconds = wait_delay_in_seconds
+        return self
 
     def add_environment(self, environment: Environment) -> 'LoggerConfigBuilder':
-        raise NotImplementedError()
+        assert isinstance(environment, Environment)
+        self.__environment = environment
+        return self
 
     def add_processor(self, processor: IProcessor) -> 'LoggerConfigBuilder':
-        raise NotImplementedError()
+        assert isinstance(processor, IProcessor)
+        self.__processors.append(processor)
+        return self
 
     def _clear(self):
-        raise NotImplementedError()
+        self.__sink = ConsoleSink()
+        self.__processors = [NullProcessor()]
+        self.__is_async = False
+        self.__async_wait_delay_in_seconds = 0
+        self.__environment = None   # Used for what?
+
+    def __build_processor(self) -> IProcessor:
+        last: IProcessor = None
+        for processor in self.__processors[::-1]:
+            if last is not None:
+                processor.set_next(last)
+            last = processor
+        return last
 
     def build(self) -> LoggerConfig:
-        raise NotImplementedError()
+        config = LoggerConfig(
+            sink=self.__sink, 
+            processor=self.__build_processor(), 
+            is_async=self.__is_async, 
+            async_wait_delay_in_seconds=self.__async_wait_delay_in_seconds
+        )
+        return config
