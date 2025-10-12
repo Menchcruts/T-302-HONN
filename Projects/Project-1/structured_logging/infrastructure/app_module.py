fe5d23d808da6291159dfdabd8d1cab92361d454
Menchcruts <joelarnar@gmail.com>
2025-10-12 17:18:09 +0000

    Added provider for Logger to AppModule

--- Projects/Project-1/structured_logging/infrastructure/app_module.py
+++ Projects/Project-1/structured_logging/infrastructure/app_module.py
@@ -1,9 +1,15 @@
+from injector import Module, provider
 
-from injector import Module
-
+from structured_logging.logger.logger import Logger
 from structured_logging.configuration.logger_config import LoggerConfig
+
+from structured_logging.command_queue.queue import Queue
 
 
 class AppModule(Module):
     def __init__(self, logger_config: LoggerConfig) -> None:
         self.__logger_config = logger_config
+
+    @provider
+    def provide_logger(self) -> Logger:
+        return Logger(self.__logger_config, Queue(self.__logger_config.async_wait_delay_in_seconds))
