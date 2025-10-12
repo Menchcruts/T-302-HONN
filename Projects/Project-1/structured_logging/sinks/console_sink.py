144191de8fc6d833bbb617c26b10c17447320ac2
Menchcruts <joelarnar@gmail.com>
2025-10-12 17:14:40 +0000

    Cleaned up ConsoleSink

--- Projects/Project-1/structured_logging/sinks/console_sink.py
+++ Projects/Project-1/structured_logging/sinks/console_sink.py
@@ -3,18 +3,4 @@ import json
 
 class ConsoleSink(ISink):
     def sink_data(self, data):
-        # data_len = len(data)
-        # i = 0
-        # strings: list[str] = ["{"]
-
-        # for key, value in data.items():
-        #     i += 1
-        #     string = f'  "{key}": "{value}"'
-        #     if i < data_len:
-        #         string += ","
-        #     strings.append(string)
-        # strings.append("}")
-
-        # print("\n".join(strings))
-
-        print(json.dumps(data, indent=2, ensure_ascii=False))
+        print(json.dumps(data, indent=2, ensure_ascii=False), flush=True)
