da023f9fa9c585fa43c034d8553a58260d3ed6f0
Menchcruts <joelarnar@gmail.com>
2025-10-12 19:33:47 +0000

    Created a logger config factory for ClientLoggerAdapter

--- Projects/Project-1/client/infrastructure/logging/logger_config_factory.py
+++ Projects/Project-1/client/infrastructure/logging/logger_config_factory.py
@@ -1,7 +1,25 @@
-from client.infrastructure.settings.settings import Settings
+from client.infrastructure.settings.settings import Settings, LoggingType
+
 from structured_logging.configuration.logger_config import LoggerConfig
 from structured_logging.logger_creation.logger_config_builder import LoggerConfigBuilder
+from structured_logging.processors.timestamp_processor import TimestampProcessor
 
 
 def create_logger_config(settings: Settings, builder: LoggerConfigBuilder) -> LoggerConfig:
-    raise NotImplementedError()
+    builder = LoggerConfigBuilder()
+
+    if settings.logging_is_async:
+        builder.as_async(settings.logging_async_delay)
+    
+    match settings.logging_type:
+        case LoggingType.CONSOLE:
+            builder.with_console_sink()
+        case LoggingType.FILE:
+            builder.with_file_sink(settings.logging_file_path)
+    
+    if settings.environment is not None:
+        builder.add_environment(settings.environment)
+
+    builder.add_processor(TimestampProcessor())
+
+    return builder.build()
